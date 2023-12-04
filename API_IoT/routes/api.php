c3fddfa016e8005acbe6f70818703d9660eda4a1
RicardoSanGon <22170038@uttcampus.edu.mx>
2023-12-04 09:57:38 -0600

    Api final v1.1

--- API_IoT/routes/api.php
+++ API_IoT/routes/api.php
@@ -4,11 +4,10 @@ use App\Http\Controllers\Historial_PaquetesController;
 use App\Http\Controllers\PaquetesController;
 use App\Http\Controllers\UsuariosController;
 use Illuminate\Http\Request;
-use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Str;
 
 Route::post('/registry/user',[UsuariosController::class,'store']);
+
 
 Route::get('/vericacionemail/{token}',[UsuariosController::class,'verification_email'])
     ->name('verificar')
@@ -18,12 +17,9 @@ Route::get('/resendemail/{token}',[UsuariosController::class,'ReenviarEmail']);
 Route::post('/login/user',[UsuariosController::class,'Login']);
 Route::post('/logout/user',[UsuariosController::class,'LogOut']);
 
-Route::prefix('v1')->group(function (){
-
+Route::middleware(['status.verify','token.verify'])->group(function (){
     Route::get('/info/pack/sensor',[PaquetesController::class,'index']);
     Route::post('/create/pack',[PaquetesController::class,'store']);
     Route::put('/update/pack/{id}',[PaquetesController::class,'update']);
-
     Route::get('/info/pack/historial/{id}',[Historial_PaquetesController::class,'index']);
-})
-->middleware(['status.verify','token.verify']);
+});
